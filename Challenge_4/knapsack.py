634cfbb5cf8f6214ba644f52423e35921d50231e
Ikey Benzaken <ikey.benz@gmail.com>
2019-08-16 19:35:34 -0400

    Added knapsack problem

--- Challenge_4/knapsack.py
+++ Challenge_4/knapsack.py
@@ -1,12 +1,19 @@
-def knapsack(C, items, n):
-    ''' A  method to determine the maximum value of the items included in the knapsack 
-        without exceeding the capacity  C
+# Got help from Jake Shams
+def knapsack(items, capacity):
+    '''inputs:
+            an array of tuples with weights and values
+            maximum capacity of the bag
+        output:
+            the max value that can fit in the bag
+        '''
+    if len(items) == 0 or capacity == 0:
+        return 0
 
-        Parameters: 
-        C= 50
-        items = (("boot", 10, 60),
-            ("tent", 20, 100),
-            ("water", 30, 120),
-            ("first aid", 15, 70))
-        Returns: max value
-    '''
+    weight, val = items[0]
+    if weight > capacity:  # If weight is too much, disregard this one
+        return knapsack(items[1:], capacity)
+
+    value_without = knapsack(items[1:], capacity)
+    value_with = knapsack(items[1:], capacity - weight) + val
+
+    return max(value_with, value_without)
